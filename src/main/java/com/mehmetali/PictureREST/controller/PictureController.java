91f1cec558518619eee0a687ab473ce513debda9
mehmetali69 <mehmetali69@eclipso.de>
2023-12-11 01:55:12 +0100

    restcontroller initial added

--- src/main/java/com/mehmetali/PictureREST/controller/PictureController.java
+++ src/main/java/com/mehmetali/PictureREST/controller/PictureController.java
@@ -1,4 +1,22 @@
 package com.mehmetali.PictureREST.controller;
 
+
+import com.mehmetali.PictureREST.model.Picture;
+import com.mehmetali.PictureREST.repository.PictureRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class PictureController {
+
+    @Autowired
+    private PictureRepository pictureRepository;
+
+    @GetMapping(value = "/pictures")
+    public Picture givePictures(){
+        return pictureRepository.findAll().get(0);
+
+
+    }
 }
